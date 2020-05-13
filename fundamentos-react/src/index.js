b1b794ddb8a6009fa752dfa53f73a68448aaf571
Felipe <ficechin@hotmail.com>
2020-05-13 15:55:13 -0300

    Project fundamentos-react developing - lesson 13.

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+import ReactDom from 'react-dom'
+import React from 'react'
+import Primeiro from "./components/basicos/Primeiro";
+
+ReactDom.render(
+    <div>
+        <Primeiro/>
+    </div>,
+    document.getElementById('root')
+)
