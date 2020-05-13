0caf8a821546773aef10d6dbe26491cc78d69eaf
Felipe <ficechin@hotmail.com>
2020-05-13 16:18:45 -0300

    Project fundamentos-react developing - lesson 15.

---
+++ fundamentos-react/src/components/basicos/ComParametro.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ComParametro(props) {
+    const status = props.nota >= 7 ? 'Aprovado':'Recuperação'
+    return (
+        <div>
+            <h2>{props.titulo}</h2>
+            <h3>{props.aluno} tem nota {props.nota} e foi <strong>{status}</strong></h3>
+        </div>
+    )
+}
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,10 +2,12 @@ import './index.css'
 import ReactDom from 'react-dom'
 import React from 'react'
 import Primeiro from "./components/basicos/Primeiro";
+import ComParametro from "./components/basicos/ComParametro";
 
 ReactDom.render(
-    <div>
+    <div id={"app"}>
         <Primeiro/>
+        <ComParametro titulo={"Segundo componente"} aluno={"Pedro"} nota={9.5}/>
     </div>,
     document.getElementById('root')
 )
