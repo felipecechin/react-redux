894eab7a1c1b7faa7c1eec4d79ddb63522b3fe15
Felipe <ficechin@hotmail.com>
2020-05-28 22:41:52 -0300

    Project redux-simples created - lesson 83.

---
+++ redux-simples/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Card from "./components/Card";
+import Intervalo from "./components/Intervalo";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Exercício React-Redux (Simples)</h1>
+            <div className={"linha"}>
+                <Intervalo/>
+            </div>
+            <div className={"linha"}>
+                <Card title={"Card 2"} green>X</Card>
+                <Card title={"Card 3"} blue>X</Card>
+                <Card title={"Card 4"} purple>X</Card>
+            </div>
+        </div>
+    );
+}
+
+export default App;
