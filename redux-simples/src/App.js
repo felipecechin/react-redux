d27ce120ed94f12642fbd42ea59387fd9dd3d8b0
Felipe <ficechin@hotmail.com>
2020-05-29 20:41:02 -0300

    Project redux-simples developing - lesson 86.

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -1,25 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import Card from "./components/Card";
 import Intervalo from "./components/Intervalo";
 import Media from "./components/Media";
 import Soma from "./components/Soma";
 import Sorteio from "./components/Sorteio";
 
 function App() {
-    const [min, setMin] = useState(100)
-    const [max, setMax] = useState(1000)
-
     return (
         <div className="App">
             <h1>Exercício React-Redux (Simples)</h1>
             <div className={"linha"}>
-                <Intervalo min={min} max={max} onMinChanged={setMin} onMaxChanged={setMax}/>
+                <Intervalo/>
             </div>
             <div className={"linha"}>
-                <Media min={min} max={max}/>
-                <Soma min={min} max={max}/>
-                <Sorteio min={min} max={max}/>
+                <Media/>
+                <Soma/>
+                <Sorteio/>
             </div>
         </div>
     );
--- redux-simples/src/components/Soma.jsx
+++ redux-simples/src/components/Soma.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import Card from "./Card";
 
 export default props => {
-    const {min, max} = props
     return (
         <Card title={"Soma dos números"} blue>
             <div>
                 <span>
                     <span>Resultado: </span>
-                    <strong>{min+max}</strong>
+                    <strong>{10}</strong>
                 </span>
             </div>
         </Card>
