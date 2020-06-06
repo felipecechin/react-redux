abd1c4d8357a9f30aa35db10f1616520ef5a4e7d
Felipe <ficechin@hotmail.com>
2020-06-05 21:22:47 -0300

    Todoapp migration to redux developing - lesson 107.

--- todo-app/frontend/src/index.jsx
+++ todo-app/frontend/src/index.jsx
@@ -4,13 +4,14 @@ import {applyMiddleware, createStore} from "redux";
 import {Provider} from 'react-redux'
 import promise from 'redux-promise'
 import multi from 'redux-multi'
+import thunk from "redux-thunk";
 
 import App from './main/app'
 import rootReducer from "./main/reducers";
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
     && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = applyMiddleware(multi, promise)(createStore)(rootReducer, devTools)
+const store = applyMiddleware(thunk, multi, promise)(createStore)(rootReducer, devTools)
 ReactDOM.render(
     <Provider store={store}>
         <App/>
--- todo-app/frontend/src/todo/todoList.jsx
+++ todo-app/frontend/src/todo/todoList.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {connect} from 'react-redux'
 import IconButton from '../template/iconButton'
 import Todo from "./todo";
+import {bindActionCreators} from "redux";
+import {markAsDone, markAsPending} from "./todoActions";
 
 const TodoList = props => {
     const renderRows = () => {
@@ -11,9 +13,9 @@ const TodoList = props => {
                 <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                 <td>
                     <IconButton style={'success'} icon={'check'} hide={todo.done}
-                                onClick={() => props.handleMarkAsDone(todo)}/>
+                                onClick={() => props.markAsDone(todo)}/>
                     <IconButton style={'warning'} icon={'undo'} hide={!todo.done}
-                                onClick={() => props.handleMarkAsPending(todo)}/>
+                                onClick={() => props.markAsPending(todo)}/>
                     <IconButton style={'danger'} icon={'trash-o'} hide={!todo.done}
                                 onClick={() => props.handleRemove(todo)}/>
                 </td>
@@ -40,4 +42,6 @@ const mapStateToProps = state => ({
     list: state.todo.list
 })
 
-export default connect(mapStateToProps)(TodoList)
+const mapDispatchToProps = dispatch => bindActionCreators({markAsDone, markAsPending}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
