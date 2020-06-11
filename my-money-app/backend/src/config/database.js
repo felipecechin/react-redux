f767e0dee6dbe2e19135c0d836abddb2ead64eef
Felipe <ficechin@hotmail.com>
2020-06-10 21:18:38 -0300

    Backend my money app developing - lesson 119.

--- my-money-app/backend/src/config/database.js
+++ my-money-app/backend/src/config/database.js
@@ -3,7 +3,13 @@ mongoose.Promise = global.Promise
 
 module.exports = mongoose.connect('BANCO', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 }).then(function () {
     console.log('MongoDB conectado')
 })
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.Number.min = "'{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "'{VALUE}' informado é maior que o limite máximo de '{MAX}'."
+mongoose.Error.messages.String.enum = "'{VALUE}' não é válido para o atributo '{PATH}'."
