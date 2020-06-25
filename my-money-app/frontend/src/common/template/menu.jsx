707600f3b7475b1c0e6b36d8cbfc131777e53a1d
Felipe <ficechin@hotmail.com>
2020-06-25 20:31:03 -0300

    Frontend my-money-app developing - lesson 139.

--- my-money-app/frontend/src/common/template/menu.jsx
+++ my-money-app/frontend/src/common/template/menu.jsx
@@ -6,7 +6,7 @@ export default props => (
     <ul className={'sidebar-menu'}>
         <MenuItem path={'#/'} label={'Dashboard'} icon={'dashboard'}/>
         <MenuTree label={'Cadastro'} icon={'edit'}>
-            <MenuItem path={'#billingCycles'} label={'Ciclos de pagamentos'} icon={'usd'}/>
+            <MenuItem path={'#/billingCycles'} label={'Ciclos de pagamentos'} icon={'usd'}/>
         </MenuTree>
     </ul>
 )
