7eaff7b5596dfe6d625ca2862fe00dd6c76e3d97
Felipe <ficechin@hotmail.com>
2020-08-18 21:29:49 -0300

    Frontend my-money-app developing - lesson 181.

--- my-money-app/frontend/src/billingCycle/creditList.jsx
+++ my-money-app/frontend/src/billingCycle/creditList.jsx
@@ -1,21 +1,51 @@
 import React, {Component} from 'react'
-import {Field} from 'redux-form'
+import {connect} from 'react-redux'
+import {bindActionCreators} from "redux";
+import {Field, arrayInsert, arrayRemove} from 'redux-form'
 import Grid from '../common/layout/grid'
+import Input from '../common/form/input'
 
 class CreditList extends Component {
+    add(index, item = {}) {
+        if (!this.props.readOnly) {
+            this.props.arrayInsert('billingCycleForm', 'credits', index, item)
+        }
+    }
+
+    remove(index) {
+        if (!this.props.readOnly && this.props.list.length>1) {
+            this.props.arrayRemove('billingCycleForm', 'credits', index)
+        }
+    }
+
     renderRows() {
-        return (
-            <tr>
+        const list = this.props.list || []
+        return list.map((item, index) => (
+            <tr key={index}>
                 <td>
-                    <Field name={'credits[0].name'} component={'input'}/>
+                    <Field name={'credits[' + index + '].name'} component={Input}
+                           placeholder={'Informe o nome'} readOnly={this.props.readOnly}/>
                 </td>
                 <td>
-                    <Field name={'credits[0].value'} component={'input'}/>
+                    <Field name={'credits[' + index + '].value'} component={Input}
+                           placeholder={'Informe o valor'} readOnly={this.props.readOnly}/>
                 </td>
                 <td>
+                    <button type={'button'} className={'btn btn-success'}
+                            onClick={() => this.add(index + 1)}>
+                        <i className={'fa fa-plus'}/>
+                    </button>
+                    <button type={'button'} className={'btn btn-warning'}
+                            onClick={() => this.add(index + 1, item)}>
+                        <i className={'fa fa-clone'}/>
+                    </button>
+                    <button type={'button'} className={'btn btn-danger'}
+                            onClick={() => this.remove(index + 1)}>
+                        <i className={'fa fa-trash-o'}/>
+                    </button>
                 </td>
             </tr>
-        )
+        ))
     }
 
     render() {
@@ -28,7 +58,7 @@ class CreditList extends Component {
                         <tr>
                             <th>Nome</th>
                             <th>Valor</th>
-                            <th>Ação</th>
+                            <th className={'table-actions'}>Ação</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -41,4 +71,6 @@ class CreditList extends Component {
     }
 }
 
-export default CreditList
+const mapDispatchToProps = dispatch => bindActionCreators({arrayInsert, arrayRemove}, dispatch)
+
+export default connect(null, mapDispatchToProps)(CreditList)
