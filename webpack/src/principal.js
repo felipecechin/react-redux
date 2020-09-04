dd016dcc5fd3d78de478769555eda623149fc267
Felipe <ficechin@hotmail.com>
2020-09-04 17:09:10 -0300

    Webpack exercises - lesson 235.

---
+++ webpack/src/principal.js
@@ -0,0 +1,12 @@
+//const Pessoa = require('./pessoa')
+
+import Pessoa from "./pessoa";
+
+//sistema de módulos ecmascript
+//usa export e import
+
+//sistema de módulos commonjs
+//usa module.exports e require
+
+const atendente = new Pessoa
+console.log(atendente.cumprimentar())
