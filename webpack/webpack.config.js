659ff855d7c34c6c4d3c390b43df7815c4ae6abf
Felipe <ficechin@hotmail.com>
2020-09-08 16:45:58 -0300

    Webpack exercises - lesson 243.

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,29 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtrackPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: "principal.js",
         path: __dirname + '/public'
+    },
+    devServer: {
+        contentBase: "./public",
+        port: 9000
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCssAssetsPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtrackPlugin({
@@ -22,6 +39,12 @@ module.exports = {
                     //'style-loader', //Adiciona CSS a DOM injetando a tag <style>
                     'css-loader', // interpreta @import, url()...
                     'sass-loader',
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
                 ]
             }
         ]
