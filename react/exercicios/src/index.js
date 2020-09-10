4360f3e628b5d65b56aa458e3b86a19608286a7a
Felipe <ficechin@hotmail.com>
2020-09-09 21:55:03 -0300

    React 16.4 exercises - lesson 249.

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import Primeiro from "./componentes/Primeiro";
+//componente com primeira letra maiuscula
+import BomDia from "./componentes/BomDia";
+
+import Multi from "./componentes/Multiplos";
+import Saudacao from "./componentes/Saudacao";
 
 //isso é JSX, React deve estar importado
 const elemento = <h1>React 2</h1>
 
-ReactDOM.render(<Primeiro/>, document.getElementById('root'))
+ReactDOM.render(<div>
+        <Saudacao tipo={"Bom dia"} nome={"João"}/>
+    </div>, document.getElementById('root')
+)
