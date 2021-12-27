f0e0654f47a88ed4d14b38ecd2aa4e813b93580a
Felipe <ficechin@hotmail.com>
2021-12-26 22:26:37 -0300

    Project navigation concluded.

--- navegacao/src/components/layout/Content.jsx
+++ navegacao/src/components/layout/Content.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom'
 import About from "../../views/examples/About";
 import Home from "../../views/examples/Home";
+import Param from "../../views/examples/Param";
+import NotFound from "../../views/examples/NotFound";
 
 const Content = props => (
     <main className={'Content'}>
         <Switch>
+            <Route exact path={'/'} component={Home}/>
             <Route path={'/about'} component={About}/>
-            <Route path={'/'} component={Home}/>
+            <Route path={'/param/:id'} component={Param}/>
+            <Route path={'*'} component={NotFound}/>
         </Switch>
     </main>
 )
--- navegacao/src/components/layout/Menu.jsx
+++ navegacao/src/components/layout/Menu.jsx
@@ -12,6 +12,15 @@ const Menu = props => (
                 <li>
                     <Link to={'/about'}>Sobre</Link>
                 </li>
+                <li>
+                    <Link to={'/param/123'}>Param #01</Link>
+                </li>
+                <li>
+                    <Link to={'/param/legal'}>Param #02</Link>
+                </li>
+                <li>
+                    <Link to={'/naoExiste'}>Não existe</Link>
+                </li>
             </ul>
         </nav>
     </aside>
